0b5b8a1f48f4ec2be4967b6d5c36ef77db245145
Adrien Haxaire <adrien.haxaire@gmail.com>
2011-05-29 22:57:21 +0200

    CG compiles

--- Solver.hs
+++ Solver.hs
@@ -20,33 +20,58 @@ import Data.Array.Repa as R
 eps :: Double
 eps = 1.0e-3
 
-{-
-
-cg :: Array DIM2 Double -> Array DIM1 Double -> Array DIM1 Double
-cg a b = if r0 <= eps then x else cg' a x r0 
+cg :: Array DIM2 Double -> Array DIM1 Double -> Array DIM1 Double 
+      -> Array DIM1 Double
+cg a x b = if norm r <= eps then x else cg' a x r r r
   where 
-    r0 = residue b a x
+    r = b - a `multiplyMV` x
+
 
 cg' :: Array DIM2 Double -> Array DIM1 Double -> Array DIM1 Double
- 
--}  
-  
--- | Residue needed to check the exit of the CG.
-residue :: Array DIM1 Double -> Array DIM2 Double -> Array DIM1 Double -> Double
-residue b a x = norm (b - a `multiplyMV` x)
+       -> Array DIM1 Double -> Array DIM1 Double
+       -> Array DIM1 Double 
+cg' a x r z p = if norm r' <= eps then x' else cg' a x' r' z' p' 
+  where 
+    alpha' = alpha r z p a
+    beta'  = beta z' r' z r
+    r' = r + R.map (*alpha') (a `multiplyMV` p)
+    z' = r' 
+    x' = x + R.map (*alpha') p 
+    p' = z'+ R.map (*beta') p 
 
+
+alpha :: Array DIM1 Double -> Array DIM1 Double 
+         -> Array DIM1 Double -> Array DIM2 Double
+         -> Double
+alpha r z p a = (r `dot` z) / (p `dot` (a `multiplyMV` p))
+
+
+beta :: Array DIM1 Double -> Array DIM1 Double 
+        -> Array DIM1 Double -> Array DIM1 Double
+        -> Double
+beta z1 r1 z0 r0 = (z1 `dot` r1) / (z0 `dot` r0)
+
+
+-- | Simple dot product
+dot ::  Array DIM1 Double -> Array DIM1 Double -> Double
+dot x y = toScalar $ R.sum (x * y)
+
+
+-- | Euclidian norm 
 norm :: Array DIM1 Double -> Double
 norm  = sqrt . toScalar . R.sum . R.map (**2) 
 
 
--- | Didn't find it in the repa lib. Computes matrix * vector.
+-- | Didn't find it in the repa lib
 multiplyMV :: Array DIM2 Double -> Array DIM1 Double -> Array DIM1 Double
 multiplyMV a x = R.sum $ a*x'
   where x' = extend (Z:.(dim::Int):.All) x
         dim = rank $ extent a
 
 
-{-
+
+{- taken from wikipedia, iterative version:
+
 x0 = 0 (or better)
 r0 = b - Ax0
 z0 = M-1 r0
@@ -65,3 +90,4 @@ k = k + 1
 end loop
 
 -}
+
