56f1991c8471595ee44593dec64263bc6c8e23e1
Adrien Haxaire <adrien.haxaire@gmail.com>
2011-06-08 21:52:43 +0200

    added inline instructions

--- Solver.hs
+++ Solver.hs
@@ -22,6 +22,7 @@ eps = 1.0e-3
 
 cg :: Array DIM2 Double -> Array DIM1 Double -> Array DIM1 Double 
       -> Array DIM1 Double
+{-# INLINE cg #-}
 cg a x b = if norm r <= eps then x else cg' a x r r r
   where 
     r = b - a `multiplyMV` x
@@ -30,6 +31,7 @@ cg a x b = if norm r <= eps then x else cg' a x r r r
 cg' :: Array DIM2 Double -> Array DIM1 Double -> Array DIM1 Double
        -> Array DIM1 Double -> Array DIM1 Double
        -> Array DIM1 Double 
+{-# INLINE cg' #-}
 cg' a x r z p = if norm r' <= eps then x' else cg' a x' r' z' p' 
   where 
     alpha' = alpha r z p a
@@ -43,30 +45,36 @@ cg' a x r z p = if norm r' <= eps then x' else cg' a x' r' z' p'
 alpha :: Array DIM1 Double -> Array DIM1 Double 
          -> Array DIM1 Double -> Array DIM2 Double
          -> Double
+{-# INLINE  alpha #-}
 alpha r z p a = (r `dot` z) / (p `dot` (a `multiplyMV` p))
 
 
 beta :: Array DIM1 Double -> Array DIM1 Double 
         -> Array DIM1 Double -> Array DIM1 Double
         -> Double
+{-# INLINE beta #-}
 beta z1 r1 z0 r0 = (z1 `dot` r1) / (z0 `dot` r0)
 
 
 -- | Simple dot product
 dot ::  Array DIM1 Double -> Array DIM1 Double -> Double
+{-# INLINE dot #-}
 dot x y = toScalar $ R.sum (x * y)
 
 
 -- | Euclidian norm 
 norm :: Array DIM1 Double -> Double
+{-# INLINE norm #-}
 norm  = sqrt . toScalar . R.sum . R.map (**2) 
 
 
 -- | Didn't find it in the repa lib
 multiplyMV :: Array DIM2 Double -> Array DIM1 Double -> Array DIM1 Double
+{-# NOINLINE multiplyMV #-}
 multiplyMV a x = R.sum $ a*x'
   where x' = extend (Z:.(dim::Int):.All) x
         dim = rank $ extent a
+
 
 
 
