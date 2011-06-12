d7be16c8598e93b6e71ba13037f848deef0106c2
Adrien Haxaire <adrien.haxaire@gmail.com>
2011-06-12 18:52:35 +0200

    added Hydraulics

--- Numeric/Funfem.hs
+++ Numeric/Funfem.hs
@@ -2,10 +2,12 @@ module Numeric.Funfem (
   module Numeric.Funfem.Elements
   ,module Numeric.Funfem.Input
   ,module Numeric.Funfem.Solver
---  ,module Numeric.Funfem.Hydraulics
+  ,module Numeric.Funfem.Stiffnesses
+  ,module Numeric.Funfem.Hydraulics
   ) where
 
 import Numeric.Funfem.Elements
 import Numeric.Funfem.Input
 import Numeric.Funfem.Solver
---import Funfem.Hydraulics
+import Numeric.Funfem.Stiffnesses
+import Numeric.Funfem.Hydraulics
