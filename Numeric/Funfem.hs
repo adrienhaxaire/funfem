db5d16ce588764e6982b095d77023ff70d242ee6
Adrien Haxaire <adrien@arch.localdomain>
2011-10-31 22:02:45 +0100

    removed repa and modules using repa. funfem can be installed again

--- Numeric/Funfem.hs
+++ Numeric/Funfem.hs
@@ -2,12 +2,8 @@ module Numeric.Funfem (
   module Numeric.Funfem.Elements
   ,module Numeric.Funfem.Input
   ,module Numeric.Funfem.Solver
-  ,module Numeric.Funfem.Stiffnesses
-  ,module Numeric.Funfem.Hydraulics
   ) where
 
 import Numeric.Funfem.Elements
 import Numeric.Funfem.Input
 import Numeric.Funfem.Solver
-import Numeric.Funfem.Stiffnesses
-import Numeric.Funfem.Hydraulics
