1c02e9aabffcaff2ecd85b378d2677ea012a13ab
Adrien Haxaire <adrien@haxaire.org>
2011-11-02 01:19:11 +0100

    change newtype to data

--- Numeric/Funfem/Vector.hs
+++ Numeric/Funfem/Vector.hs
@@ -13,41 +13,56 @@
 
 module Numeric.Funfem.Vector where
 
-newtype Vector v = Vector [v] deriving (Eq, Show)
+data Vector = Vector [Double] 
+            deriving (Eq, Ord, Show)
 
-instance Num v => Num (Vector v) where
-  
-  Vector [a,b] + Vector [a',b'] = Vector [a+a',b+b']
-  Vector [] + Vector [a,b] = Vector [a,b]
-  Vector [a,b] + Vector [] = Vector [a,b]
-  Vector [] + Vector [] = Vector []
-
-  Vector [a,b] * Vector [a',b'] = Vector [a*a',b*b']
-  Vector [] * Vector [_] = Vector []
-  Vector [_] * Vector [] = Vector []
-  Vector [] * Vector [] = Vector []
-
+instance Num Vector where
+  Vector a + Vector b = Vector [a' + b' | a' <- a, b' <- b]
+  Vector a * Vector b = Vector [a' * b' | a' <- a, b' <- b]
   negate (Vector [a,b]) = Vector [-a,-b]
-    
   fromInteger n = Vector [fromInteger n]
 
-
-fromVector :: Vector v -> [v]
+fromVector :: Vector -> [Double]
 fromVector (Vector v) = v
 
-fromList :: [v] -> Vector v
+fromList :: [Double] -> Vector 
 fromList v = Vector v
 
-dot_product :: Num v => Vector v -> Vector v -> v
+size :: Vector -> Int
+{-# INLINE size #-}
+size = length . fromVector
+
+dot_product :: Vector -> Vector -> Double
+{-# INLINE dot_product #-}
 dot_product v1 v2 = foldl (+) 0 $ fromVector (v1*v2)
 
 -- | Infix dot product 
-(.*) :: Num v => Vector v -> Vector v -> v
+(.*) :: Vector -> Vector -> Double
+{-# INLINE (.*) #-}
 v1 .* v2 = foldl (+) 0 $ fromVector (v1*v2)
 
-concat :: Vector v -> Vector v -> Vector v
+concat :: Vector -> Vector -> Vector 
+{-# INLINE concat #-}
 concat v1 v2 = Vector (fromVector v1 Prelude.++ fromVector v2)
 
 -- | Infix vector concatenation
-(++) :: Vector v -> Vector v -> Vector v
+(++) :: Vector -> Vector -> Vector
+{-# INLINE (++) #-}
 v1 ++ v2 = Vector (fromVector v1 Prelude.++ fromVector v2)
+
+
+data Matrix = Matrix [Vector]
+            deriving (Eq, Ord, Show)
+
+fromMatrix :: Matrix -> [Vector]
+fromMatrix (Matrix m) = m
+
+fromVectors :: [Vector] -> Matrix
+fromVectors v = Matrix v
+
+dot :: Matrix -> Vector -> Vector
+{-# INLINE dot #-}
+dot m v = fromList $ dot' (fromMatrix m) v 
+  where
+    dot' (w:ws) v' = [w .* v'] Prelude.++ dot' ws v'
+    dot' [] _ = []  
