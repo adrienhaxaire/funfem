c773574a58ec72a245a9d3b11343f40084c2bbc7
Adrien Haxaire <adrien@haxaire.org>
2011-11-03 00:34:19 +0100

    added Arbitrary instance for Vector and a dummy tests

--- tests/QC.hs
+++ tests/QC.hs
@@ -4,8 +4,9 @@ import Test.QuickCheck.All
 
 
 import Numeric.Funfem.Elements as E
+import Numeric.Funfem.Vector
 
-
+-- Elements
 instance Arbitrary E.Node where
   arbitrary = do
     x1 <- choose (-100.0, 100.0) :: Gen Double
@@ -13,6 +14,7 @@ instance Arbitrary E.Node where
     n1 <- choose (1, 100) :: Gen Int
     return (E.Node (x1, y1) n1)                         
 
+prop_nodeFromExtractor :: E.Node -> Bool
 prop_nodeFromExtractor n = (E.Node (nodeCoordinates n) (nodeNumber n)) == n
 
 
@@ -22,6 +24,7 @@ instance Arbitrary E.Property where
     value <- choose (-1.0e31, 1.0e31) :: Gen Double
     return (E.Property name value)
 
+prop_propertyFromExtractor :: E.Property -> Bool
 prop_propertyFromExtractor p = (E.Property (propName p) (propValue p)) == p
 
 
@@ -33,6 +36,7 @@ instance Arbitrary E.Material where
     number <- arbitrary
     return (E.Material name props number)
 
+prop_materialFromExtractor :: E.Material -> Bool
 prop_materialFromExtractor m = (E.Material (matName m) (matProperties m) (matNumber m)) == m
 
 
@@ -44,7 +48,20 @@ instance Arbitrary E.Element where
     material <- arbitrary
     return (E.Element nodes number material)
     
+prop_elementFromExtractor :: E.Element -> Bool    
 prop_elementFromExtractor e = (E.Element (elemNodes e) (elemNumber e) (elemMaterial e)) == e    
 
+-- Vectors
+instance Arbitrary Vector where
+  arbitrary = do
+    len <- choose (0,100) :: Gen Int
+    list <- vectorOf len arbitrary
+    return (Vector list)
 
+prop_double_add_same_size :: Vector -> Bool  
+prop_double_add_same_size v = size (v + v) == size v
+
+
+
+main :: IO Bool
 main = $(quickCheckAll)
