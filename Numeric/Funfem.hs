effa3437e2b803265105e2653d0e747756f4f704
adrienhaxaire <adrien@haxaire.org>
2011-11-06 18:37:36 +0100

    exporting ShapeFunctions module

--- Numeric/Funfem.hs
+++ Numeric/Funfem.hs
@@ -3,9 +3,11 @@ module Numeric.Funfem (
   ,module Numeric.Funfem.Vector
   ,module Numeric.Funfem.Input
   ,module Numeric.Funfem.Solver
+  ,module Numeric.Funfem.ShapeFunctions
   ) where
 
 import Numeric.Funfem.Elements
 import Numeric.Funfem.Vector
 import Numeric.Funfem.Input
 import Numeric.Funfem.Solver
+import Numeric.Funfem.ShapeFunctions
