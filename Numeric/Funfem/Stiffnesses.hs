83c498c9ea8daee109eb9bbc4765303c71c447e3
adrienhaxaire <adrien@haxaire.org>
2011-11-14 22:33:30 +0100

    finished assembly of global stiffness matrix

--- Numeric/Funfem/Stiffnesses.hs
+++ Numeric/Funfem/Stiffnesses.hs
@@ -11,26 +11,39 @@
 ----------------------------------------------------------------------------------
 --
 
-module Numeric.Funfem.Stiffnesses where
+module Numeric.Funfem.Stiffnesses (toGlobalMatrix) where
 
 import Data.List as L hiding (transpose)
 import Data.Map as M
 
 import Numeric.Funfem.Elements
+import Numeric.Funfem.Vector as V 
 import Numeric.Funfem.Matrix 
-import Numeric.Funfem.ShapeFunctions
 
 type Index = (Int,Int)
 type Stiffness = M.Map (Int,Int) Double 
 
--- todo : convert stiffness to matrix
--- add init
+-- | Assembles the global stiffness matrix from the elementary
+-- stiffness constructor and the target list of elements
+toGlobalMatrix :: (Element -> Matrix) -> [Element] -> Matrix
+toGlobalMatrix elemStiff els = toMatrix $ toGlobal elemStiff els
 
-initialize :: Stiffness
-initialize = M.singleton (1,1) 0.0 :: Stiffness 
 
 toGlobal :: (Element -> Matrix) -> [Element] ->  Stiffness
 toGlobal elemStiff els = M.unions $ L.map (elemToGlobal elemStiff) els
+
+
+toMatrix :: Stiffness -> Matrix
+toMatrix s = fromVectors $ vs 
+  where
+    vs = [V.fromList [at (i,j) s | j <- [1..smax]] | i <- [1..smax]]
+    larger = fst $ findMax s
+    smax = max (fst larger) (snd larger)
+    
+
+at :: Index -> Stiffness -> Double
+at index s = case M.lookup index s of {Just val -> val; Nothing -> 0.0}
+
 
 elemToGlobal :: (Element -> Matrix) -> Element -> Stiffness
 elemToGlobal elemStiff el = indexToGlobal localIndices globalIndices matrix initialize
@@ -40,7 +53,7 @@ elemToGlobal elemStiff el = indexToGlobal localIndices globalIndices matrix init
     nbNodes = length nodes
     globalIndices = [(i,j) | i <- nodes, j <- nodes]
     localIndices = [(i,j) | i <- [1..nbNodes], j <- [1..nbNodes]]
-
+    initialize = M.singleton (1,1) 0.0 :: Stiffness
 
 indexToGlobal :: [Index] -> [Index] -> Matrix -> Stiffness -> Stiffness
 indexToGlobal [] _ _ s = s  
@@ -55,19 +68,6 @@ toStiffness (i,j) val s
   | i <= 0    = s
   | j <= 0    = s
   | otherwise = M.insertWith' (+) (i,j) val s 
-
-
---toMatrix :: Stiffness -> Matrix
---toMatrix s = fromVectors $ 
-
-
-
--- only here for development
-elementaryStiffness :: Element -> Matrix
-elementaryStiffness el = (transpose $ tri3' el) * (permeability `multSM` (tri3' el))
-  where
-    permeability = matPropertyFromName mat "permeability"
-    mat = elemMaterial el
 
 
 -- [Element [Node (0.0,0.0) 1,Node (1.0,0.0) 2,Node (1.0,1.0) 3] 1 (Material "sand" [Property "permeability" 1.0] 1),Element [Node (1.0,0.0) 2,Node (1.0,1.0) 3,Node (0.0,1.0) 4] 2 (Material "sand" [Property "permeability" 1.0] 1)]
