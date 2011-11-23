b3e434d48f3955a7a9b90dcffa8cbc6237b5ed17
adrienhaxaire <adrien@haxaire.org>
2011-11-23 22:27:12 +0100

    added upper decomposition, lower coming soon

--- Numeric/Funfem/Solver.hs
+++ Numeric/Funfem/Solver.hs
@@ -11,11 +11,12 @@
 ----------------------------------------------------------------------------------
 --
 
-module Numeric.Funfem.Solver (cg) where
+module Numeric.Funfem.Solver where
 
 import Numeric.Funfem.Vector
 import Numeric.Funfem.Matrix
 
+import qualified Data.List as L
 
 eps :: Double
 eps = 1.0e-3
@@ -30,10 +31,35 @@ cg a x b = if norm r <= eps then x else cg' a x r r r
 cg' :: Matrix -> Vector -> Vector -> Vector -> Vector -> Vector
 cg' a x r z p = if norm r' <= eps then x' else cg' a x' r' z' p'
   where
-    alpha = (r .* z) / (p .* (multMV a p))
+    alpha = (r .* z) / (p .* multMV a p)
     beta = (z' .* r') / (z .* r)
     x' = x + vmap (*alpha) p
     r' = r - vmap (*alpha) (multMV a p)
     z' = r'    
     p' = z'+ vmap (*beta) p
+
+-- | LU decomposition and back substitution
+
+-- divHead (r1:r2:rs) = r2 - map headRatios r1
+
+upper [] = []
+upper l = (L.head upped) : upper (L.tail minor)
+  where
+    upped = up l
+    minor = [L.tail u | u <- upped]
+
+up [] = []
+up (r:rs) = r : up' r rs 
+
+up' _ [] = []
+up' r (l:ls) = zipWith (-) l (L.map (*h) r) : up' r ls
+  where
+    h = L.head l / L.head r 
+    
+  
+  
+  
+-- lij = aji/aii
+
+
 
