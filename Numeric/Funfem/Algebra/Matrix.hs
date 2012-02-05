bf06fd8514a4cab21e19d1547648b19a5a42fcd4
adrienhaxaire <adrien@haxaire.org>
2012-02-05 23:40:15 +0100

    reorganized Algebra module

---
+++ Numeric/Funfem/Algebra/Matrix.hs
@@ -0,0 +1,41 @@
+
+module Numeric.Funfem.Algebra.Matrix where
+
+import Numeric.Funfem.Algebra.Vector
+import qualified Data.Vector as V
+
+type Matrix = V.Vector Vector 
+
+headColumn :: Matrix -> Vector
+headColumn = V.map V.head
+
+tailColumns :: Matrix -> Matrix
+tailColumns = V.map V.tail
+
+headRow :: Vector -> Double
+headRow = V.head
+
+tailRows :: Vector -> Vector
+tailRows = V.tail
+
+nullMatrix :: Matrix -> Bool
+nullMatrix = V.null . V.head 
+
+transpose :: Matrix -> Matrix
+transpose m | nullMatrix m = V.empty
+            | otherwise    = V.fromList [headColumn m] V.++ transpose (tailColumns m)
+
+multMV :: Matrix -> Vector -> Vector
+multMV m v = V.map (dotProd v) m
+
+multMM :: Matrix -> Matrix -> Matrix
+multMM m n = V.map row m 
+  where
+    row r = V.map (dotProd r) tn
+    tn = transpose n
+
+dim :: Matrix -> (Int, Int)
+dim m = (V.length m,V.length $ V.head m)
+
+isSquare :: Matrix -> Bool
+isSquare m = let (rows, cols) = dim m in rows == cols
---
+++ Numeric/Funfem/Algebra/Vector.hs
@@ -0,0 +1,24 @@
+
+module Numeric.Funfem.Algebra.Vector where
+
+import qualified Data.Vector as V
+
+type Vector = V.Vector Double
+
+dotProd :: Vector -> Vector -> Double
+dotProd v w = V.sum $ V.zipWith (*) v w
+
+(.*) :: Vector -> Vector -> Double
+v .* w = V.sum $ V.zipWith (*) v w
+
+norm :: Vector -> Double
+norm v = sqrt $ v .* v
+
+instance Num Vector where
+  negate = V.map negate 
+  abs = V.map abs
+  fromInteger = undefined
+  signum = V.map signum 
+  (+) = V.zipWith (+)
+  (*) = V.zipWith (*)
+
