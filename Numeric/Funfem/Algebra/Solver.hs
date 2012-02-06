c36767ec50edb06096010cfb8a47815e22e3181f
adrienhaxaire <adrien@haxaire.org>
2012-02-06 22:57:11 +0100

    reorganized solver module to allow LU solver in a separate file

--- Numeric/Funfem/Algebra/Solver.hs
+++ Numeric/Funfem/Algebra/Solver.hs
@@ -12,30 +12,7 @@
 --
 
 module Numeric.Funfem.Algebra.Solver (
-  cg
+  module Numeric.Funfem.Algebra.Solver.CG
   ) where
 
-
-import qualified Data.Vector as V
-
-import Numeric.Funfem.Algebra.Vector
-import Numeric.Funfem.Algebra.Matrix
-
--- | Solves Ax = b.
-cg :: Matrix -> Vector -> Vector
-cg a b = cg' a x0 b b b
-  where
-    x0 = V.replicate (V.length a) 0.0
-    
-cg' :: Matrix -> Vector -> Vector -> Vector -> Vector -> Vector
-cg' a x r z p = if norm r' <= eps then x' else cg' a x' r' z' p'
-  where
-    alpha = (r .* z) / (p .* multMV a p)
-    beta = (z' .* r') / (z .* r)
-    x' = x + V.map (*alpha) p
-    r' = r - V.map (*alpha) (multMV a p)
-    z' = r' -- placeholder for preconditioner  
-    p' = z'+ V.map (*beta) p
-    
-eps :: Double
-eps = 1.0e-3
+import Numeric.Funfem.Algebra.Solver.CG
---
+++ Numeric/Funfem/Algebra/Solver/CG.hs
@@ -0,0 +1,39 @@
+---------------------------------------------------------------------------------- 
+-- |
+-- Module : CG
+-- Copyright : (c) Adrien Haxaire 2012
+-- Licence : BSD3
+--
+-- Maintainer : Adrien Haxaire <adrien@funfem.org>
+-- Stability : experimental
+-- Portabilty : not tested
+--
+----------------------------------------------------------------------------------
+--
+
+module Numeric.Funfem.Algebra.Solver.CG (
+  cg
+  ) where
+
+
+import qualified Data.Vector as V
+
+import Numeric.Funfem.Algebra.Vector
+import Numeric.Funfem.Algebra.Matrix
+
+-- | Solves Ax = b.
+cg :: Matrix -> Vector -> Vector
+cg a b = cg' a x0 b b b
+  where
+    x0 = V.replicate (V.length a) 0.0
+    
+cg' :: Matrix -> Vector -> Vector -> Vector -> Vector -> Vector
+cg' a x r z p = if norm r' <= eps then x' else cg' a x' r' z' p'
+  where
+    alpha = (r .* z) / (p .* multMV a p)
+    beta = (z' .* r') / (z .* r)
+    x' = x + V.map (*alpha) p
+    r' = r - V.map (*alpha) (multMV a p)
+    z' = r' -- placeholder for preconditioner  
+    p' = z'+ V.map (*beta) p
+    eps = 1.0e-3
