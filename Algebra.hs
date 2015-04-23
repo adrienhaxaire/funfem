497e9be6f104261fd620caa1794e687d02bfa844
Adrien Haxaire <adrien@haxaire.org>
2015-04-24 00:00:18 +0200

    First draft at conjugate gradient. Doesn't converge, but at least runs in constant space!

--- Algebra.hs
+++ Algebra.hs
@@ -2,11 +2,21 @@ module Algebra where
 
 import qualified Data.Vector as V
 
+instance Num a => Num (V.Vector a) where
+   negate = V.map negate
+   u + v = V.zipWith (+) u v
+   u * v = V.zipWith (*) u v
+   abs = V.map abs
+    
 data Matrix a = Matrix { rows :: V.Vector (V.Vector a)
                        , rowsInit :: V.Vector Int}
               deriving (Eq, Ord, Show)
+                      
+instance Functor Matrix where
+    fmap f (Matrix rs is) = Matrix { rows = V.map (V.map f) rs
+                                   , rowsInit = is}
+            
 
-                       
 fromList :: [V.Vector a] -> Matrix a
 fromList as = Matrix { rows = V.fromList as
                      , rowsInit = V.replicate (length as) 0}
@@ -17,6 +27,9 @@ row a i = (rows a) V.! i
 rowInit :: Matrix a -> Int -> Int
 rowInit m i = (rowsInit m) V.! i
 
+size :: Matrix a -> Int
+size = V.length . rows
+
 inner :: Num a => V.Vector a -> V.Vector a -> a
 inner u v = V.sum $ V.zipWith (*) u v
 
@@ -24,11 +37,14 @@ infixl 9 !.
 (!.) :: Num a => V.Vector a -> V.Vector a -> a
 u !. v = inner u v
 
+norm :: (Num a, Floating a) => V.Vector a -> a
+norm v = sqrt $ inner v v
+
 multMV :: Num a => Matrix a -> V.Vector a -> V.Vector a
 multMV a v = V.fromList $ go [] 0
     where
-      n = V.length $ rows a 
-      go xs i | i == n    = reverse xs 
+      n = V.length $ rows a
+      go xs i | i == n    = reverse xs
               | otherwise = go (x:xs) (i + 1)
               where
                 x = inner r $ V.slice start end v
--- Solver.hs
+++ Solver.hs
@@ -1,35 +1,40 @@
 module Solver where
 
-import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector as V
+    
+import Algebra
 
-type Vector = V.Vector Double
-type Row = Int
-type Col = Int
-type Size = Int
-type Idx = Int
 
--- sparse matrices for later optimisation    
-data Matrix = Matrix { matrixData :: Vector
-                     , matrixSize :: Size}
-            deriving (Eq, Ord, Show)
-
-matrix :: Size -> Matrix
-matrix n = Matrix { matrixData = V.replicate n 0
-                  , matrixSize = n}
-
-fromVector :: Vector -> Matrix
-fromVector v = Matrix { matrixData = v
-                      , matrixSize = n}
+cg :: Matrix Double -> V.Vector Double -> V.Vector Double
+cg a b = go r0 p0 u0
     where
-      n = floor . sqrt . fromIntegral . V.length $ v
+    u0 = V.replicate (size a) 0
+    r0 = b
+    p0 = r0
 
-idx :: Size -> (Row, Col) -> Idx
-idx n (i, j) = n * i + j
-               
-row :: Row -> Matrix -> Vector
-row i m = V.slice (idx n (i,0)) n (matrixData m)
-    where
-      n = matrixSize m
-          
+    tolerance :: Double
+    tolerance = 1.0e-4
+
+    go u r p = if norm r' < tolerance then u
+               else go u' r' p'
+                   where
+                     alpha = (r !. r) / ((a !* p) !. p)
+                     u' = u - fmap (* alpha) p
+                     r' = r - fmap (* alpha) (a !* p)
+                     beta = (r' !. r') / (r !. r)
+                     p' = r' + fmap (* beta) p
+
+
+v1, v2, v3 :: V.Vector Double
+v1 = V.fromList [7,4,6]
+v2 = V.fromList [4,8,4]
+v3 = V.fromList [6,4,9]
+
+a :: Matrix Double
+a = fromList [v1, v2, v3]
+
+unitMatrix :: Matrix Double    
+unitMatrix = fromList [V.fromList [1,0,0], V.fromList [0,1,0], V.fromList [0,0,1]]
+
 
 
