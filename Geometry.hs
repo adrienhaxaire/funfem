17834c4a0eac71a605de15e01ddcdd39669d2e90
Adrien Haxaire <adrien@haxaire.org>
2015-04-28 22:26:48 +0200

    Created simple data types for points and nodes.

--- Geometry.hs
+++ Geometry.hs
@@ -4,18 +4,23 @@
 module Geometry where
 
 import GHC.Generics
-import Control.Applicative
-import Control.Monad
-import Data.Binary as B
-import Data.Aeson as A
+import Data.Aeson
 
 data Point = Point1D {x :: Double}
            | Point2D {x :: Double, y :: Double}
            | Point3D {x :: Double, y :: Double, z :: Double}
              deriving (Eq, Ord, Show, Generic)
 
-                      
+instance ToJSON Point
+instance FromJSON Point
+                     
 data Node = Node { nodeNumber :: Int
-                 , nodePoint :: Point }
-          deriving (Eq, Ord, Show, Generic)
+                 , nodePoint :: Point}
+            deriving (Eq, Ord, Show, Generic)
+
+instance ToJSON Node
+instance FromJSON Node
+
+
+
 
