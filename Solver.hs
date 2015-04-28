2bdcc81e86c5f28b3564377151f5d12cbde90ff9
Adrien Haxaire <adrien@haxaire.org>
2015-04-28 20:59:37 +0200

    Factored out the tolerance

--- Solver.hs
+++ Solver.hs
@@ -1,18 +1,15 @@
 module Solver where
 
 import qualified Data.Vector as V
-    
+
 import Algebra
 
-cg :: Matrix Double -> V.Vector Double -> V.Vector Double
-cg a b = go u0 r0 p0
+cgWithTolerance :: Matrix Double -> V.Vector Double -> Double -> V.Vector Double
+cgWithTolerance a b tolerance = go u0 r0 p0
     where
     u0 = V.replicate (size a) 0
     r0 = b
     p0 = r0
-
-    tolerance :: Double
-    tolerance = 1.0e-4
 
     go u r p = if norm r < tolerance then u
                else go u' r' p'
@@ -22,5 +19,11 @@ cg a b = go u0 r0 p0
                      r' = r - fmap (* alpha) (a !* p)
                      beta = (r' !. r') / (r !. r)
                      p' = r' + fmap (* beta) p
+
+
+cg :: Matrix Double -> V.Vector Double -> V.Vector Double
+cg a b = cgWithTolerance a b 1.0e-4
+
+
 
 
