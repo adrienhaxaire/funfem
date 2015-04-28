d961c07dcccc1b2d8480951cf9f9c9aeb24cb685
Adrien Haxaire <adrien@haxaire.org>
2015-04-28 22:36:00 +0200

    Added some accessors for the Point and Node coordinates.

--- Geometry.hs
+++ Geometry.hs
@@ -6,20 +6,34 @@ module Geometry where
 import GHC.Generics
 import Data.Aeson
 
-data Point = Point1D {x :: Double}
-           | Point2D {x :: Double, y :: Double}
-           | Point3D {x :: Double, y :: Double, z :: Double}
+data Point = Point1D {xp :: Double}
+           | Point2D {xp :: Double, yp :: Double}
+           | Point3D {xp :: Double, yp :: Double, zp :: Double}
              deriving (Eq, Ord, Show, Generic)
 
 instance ToJSON Point
 instance FromJSON Point
-                     
+
+
 data Node = Node { nodeNumber :: Int
                  , nodePoint :: Point}
             deriving (Eq, Ord, Show, Generic)
 
 instance ToJSON Node
 instance FromJSON Node
+
+-- | The `x` coordinate of the `Node`.
+xn :: Node -> Double
+xn (Node _ p) = xp p
+
+-- | The `y` coordinate of the given `Node`. An exception is thrown in the case of a dimension mismatch.
+yn :: Node -> Double
+yn (Node _ p) = yp p
+
+-- | The `z` coordinate of the given `Node`. An exception is thrown in the case of a dimension mismatch.
+zn :: Node -> Double
+zn (Node _ p) = zp p
+
 
 
 
